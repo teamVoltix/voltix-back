6504567e1c11024a42e15463594742ac1fb7215a
Angie <eguzkia6@gmail.com>
2024-11-17 19:44:19 +0100

    update theory.md

--- site_app/authentication/urls.py
+++ site_app/authentication/urls.py
@@ -6,7 +6,7 @@ from .protected_endpoint_view import ProtectedEndpointView
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('registro/', views.registro_usuario, name='registro_usuario'),
+    path('register/', views.registro_usuario, name='registro_usuario'),
     path('login/', LoginView.as_view(), name='login'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('protected-endpoint/', ProtectedEndpointView.as_view(), name='protected_endpoint'),
