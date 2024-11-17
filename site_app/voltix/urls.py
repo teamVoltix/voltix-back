91565ea09291c0db04f3402237cab8045bfca4a6
Denis <denis9diaz@hotmail.com>
2024-11-17 11:06:14 +0100

    creado endpoint GET usuarios

--- site_app/voltix/urls.py
+++ site_app/voltix/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('usuarios/', views.get_all_users, name='get_all_users'),
 ]
--- site_app/voltix/views.py
+++ site_app/voltix/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from voltix.models import Usuario
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
-
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Bienvenido a Voltix.")
+
+@csrf_exempt
+def get_all_users(request):
+    if request.method == 'GET':
+        try:
+            # Obtener todos los usuarios
+            usuarios = Usuario.objects.all().values(
+                'user_id', 'fullname', 'dni', 'email', 'created_at', 'updated_at'
+            )
+            usuarios_list = list(usuarios)
+
+            # Respuesta JSON con los usuarios
+            return JsonResponse({
+                "message": "Usuarios obtenidos exitosamente",
+                "usuarios": usuarios_list
+            }, status=200)
+
+        except Exception as e:
+            return JsonResponse({"error": f"Ocurrió un error: {str(e)}"}, status=500)
+
+    return JsonResponse({"error": "Método no permitido"}, status=405)
