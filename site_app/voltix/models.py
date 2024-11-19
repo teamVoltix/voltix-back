8553f9f9fe0a3d23961d119ef03c070337f3c588
Angie <eguzkia6@gmail.com>
2024-11-19 14:02:37 +0100

    recheck superadmin

--- site_app/voltix/models.py
+++ site_app/voltix/models.py
@@ -15,6 +15,10 @@ class UserManager(BaseUserManager):
     def create_superuser(self, dni, fullname, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
         return self.create_user(dni, fullname, email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
