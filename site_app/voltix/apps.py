38535befa9af53cc59a007f02ff260929612ee65
Angie <eguzkia6@gmail.com>
2024-11-24 20:52:00 +0100

    fix the profile patch signal, added looger and move def inside appconfig

--- site_app/voltix/apps.py
+++ site_app/voltix/apps.py
@@ -5,5 +5,10 @@ class VoltixConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'voltix'
     
-def ready(self):
-    import voltix.signals
+    def ready(self):
+        import voltix.signals
+
+    def ready(self):
+        print("VoltixConfig ready() called")
+        import voltix.signals
+
