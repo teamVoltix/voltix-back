66afade7a3d249a06a39f1700b88de7f9ba8645e
Angie <eguzkia6@gmail.com>
2024-11-24 20:34:27 +0100

    updates view of admin, added fields to see good JSON data

--- site_app/authentication/urls.py
+++ site_app/authentication/urls.py
@@ -13,18 +13,17 @@ from .views import protected_view, logout_view, change_password_view, password_r
 
 urlpatterns = [
     path("", views.index, name="index"),
-    # path('register/', views.registro_usuario, name='registro_usuario'),
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-
-    # path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-    path('protected/', protected_view, name='protected'),
-    # path('profile/', user_profile, name='user_profile'),
     path('logout/', logout_view, name='logout'),
     path('profile/change-password/', change_password_view.as_view(), name='change_password'),
     path('password/reset/', password_reset_request_view, name='password_reset_request'),
     path('password/reset/<uidb64>/<token>/', password_reset_view, name='password_reset'),
+
+
+    # paths de tokens and to do cheks
+    # path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('protected/', protected_view, name='protected'),
 ]
