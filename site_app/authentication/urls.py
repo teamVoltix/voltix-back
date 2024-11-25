99dc326ed98f1492c207bef609554e31e26b6bd5
EIAHRJAY <er70302409@gmail.com>
2024-11-25 09:38:28 +0100

    conflitos resueltos

--- site_app/authentication/urls.py
+++ site_app/authentication/urls.py
@@ -12,14 +12,8 @@ from .views import protected_view, logout_view, change_password_view, password_r
 
 
 urlpatterns = [
-<<<<<<< HEAD
-    #path("", views.index, name="index"),
-    # path('register/', views.registro_usuario, name='registro_usuario'),
-    path('', UserRegistrationView.as_view(), name='register'), #hace con que sea la primera vista
-=======
     path("", views.index, name="index"),
     path('register/', UserRegistrationView.as_view(), name='register'),
->>>>>>> 66afade (updates view of admin, added fields to see good JSON data)
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', logout_view, name='logout'),
     path('profile/change-password/', change_password_view.as_view(), name='change_password'),
