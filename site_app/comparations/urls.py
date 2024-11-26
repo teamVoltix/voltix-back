6038e28af7b715267e7169ff6899e0ae749ebd50
EIAHRJAY <er70302409@gmail.com>
2024-11-26 14:02:00 +0100

    ruta POST

--- site_app/comparations/urls.py
+++ site_app/comparations/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('compare/', views.compare_and_save, name='compare_and_save'),
+    path('compare/', views.compare_invoice_and_measurement, name='compare_and_save'),
+    
 ]
