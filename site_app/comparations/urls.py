4b9a99c53aa2d48e4b33832af314d5144106d0e2
EIAHRJAY <er70302409@gmail.com>
2024-11-29 20:26:15 +0100

    add file  userComparison

--- site_app/comparations/urls.py
+++ site_app/comparations/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('compare/', views.compare_invoice_and_measurement, name='compare_and_save'),
-    
+    path('comparisons/<int:comparison_id>/', views.UserComparisonDetailView.as_view(), name='user_comparison_detail'),
 ]
