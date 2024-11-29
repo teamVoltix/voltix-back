1da2477c4e205f348c9ec059027a50cf1d25400c
Angie <eguzkia6@gmail.com>
2024-11-29 17:37:29 +0100

    aplicated function from utils.py for userMeasurementsListview

--- site_app/measurements/serializers.py
+++ site_app/measurements/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from voltix.models import Measurement
 
 class MeasurementSerializer(serializers.ModelSerializer):
+    comparison_status = serializers.CharField(read_only=True)
+
     class Meta:
         model = Measurement
         fields = '__all__'
