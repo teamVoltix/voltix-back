9c9a90b8684ad8f8e504df82bbad881170009026
Angie <eguzkia6@gmail.com>
2024-12-02 15:03:00 +0100

    created endpoint for measurement por id

--- site_app/measurements/urls.py
+++ site_app/measurements/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index, get_all_measurements
+from .views import index, get_all_measurements, MeasurementDetailView
 from .userMeasurementListview import UserMeasurementListView
 
 urlpatterns = [
     #path('', index, name='measurements_index'), 
     path('', UserMeasurementListView.as_view(), name='get_user_measurements'),  # Mediciones del usuario autenticado
     path('all/', get_all_measurements, name='get_all_measurements'),  # Todas las mediciones
+    path('<int:measurement_id>/', MeasurementDetailView.as_view(), name='measurement_detail'),
 ]
