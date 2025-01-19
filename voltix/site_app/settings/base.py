503ec9e8dacafbb0fe16a7ad7bf7083d50969dce
Angie <eguzkia6@gmail.com>
2025-01-20 00:51:48 +0100

    change: add to requirements django-crontab and deleted celery

--- voltix/site_app/settings/base.py
+++ voltix/site_app/settings/base.py
@@ -133,15 +133,3 @@ CRONJOBS = [
 
 
 
-# Configuración de Celery
-# CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-# CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
-# CELERY_ACCEPT_CONTENT = ['json']
-# CELERY_TASK_SERIALIZER = 'json'
-
-# Configuración de Cloudinary
-# CLOUDINARY = {
-#     'cloud_name': os.getenv('CLOUDINARY_CLOUD_NAME'),
-#     'api_key': os.getenv('CLOUDINARY_API_KEY'),
-#     'api_secret': os.getenv('CLOUDINARY_API_SECRET'),
-# }
