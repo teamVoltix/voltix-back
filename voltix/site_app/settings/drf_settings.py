84364d1130fa9db865bb691e669df486b11fc02c
Angie <eguzkia6@gmail.com>
2025-01-21 17:29:57 +0100

    comment throttle classes and rates in drf_settings.py

--- voltix/site_app/settings/drf_settings.py
+++ voltix/site_app/settings/drf_settings.py
@@ -17,14 +17,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_THROTTLE_CLASSES': [
-        'rest_framework.throttling.UserRateThrottle',
-        'rest_framework.throttling.AnonRateThrottle',
-    ],
-    'DEFAULT_THROTTLE_RATES': {
-        'user': '5/hour',
-        'anon': '2/hour',
-    },
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.UserRateThrottle',
+    #     'rest_framework.throttling.AnonRateThrottle',
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'user': '5/hour',
+    #     'anon': '2/hour',
+    # },
 }
 
 
